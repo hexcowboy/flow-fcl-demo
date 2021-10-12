317881a519420940e291332478ccbb1bf9c2bda6
hexcowboy <hex@cowboy.dev>
2021-10-12 08:06:30 -0500

    add newline

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import * as fcl from '@onflow/fcl'
-import * as types from '@onflow/types'
+import * as fcl from "@onflow/fcl";
+import * as types from "@onflow/types";
 
-fcl.config()
-  .put('accessNode.api', process.env.REACT_APP_FLOW_ACCESS_NODE)
-  .put('challenge.handshake', process.env.REACT_APP_FLOW_WALLET_DISCOVERY)
-  .put('discovery.wallet', process.env.REACT_APP_FLOW_WALLET_DISOVERY)
+fcl
+  .config()
+  .put("accessNode.api", process.env.REACT_APP_FLOW_ACCESS_NODE)
+  .put("challenge.handshake", process.env.REACT_APP_FLOW_WALLET_DISCOVERY)
+  .put("discovery.wallet", process.env.REACT_APP_FLOW_WALLET_DISOVERY);
 
 function App() {
   const payWithFlow = async () => {
     try {
-      await fcl.authenticate()
+      await fcl.authenticate();
       const response = await fcl.send([
         fcl.transaction`import FungibleToken from 0xFungibleToken
 import FlowToken from 0xFlowToken
@@ -27,7 +28,8 @@ transaction(amount: UFix64) {
             ?? panic("Could not borrow receiver reference to the recipient's Vault")
         receiverRef.deposit(from: <-self.sentVault)
     }
-}`,
+}
+`,
         fcl.args([
           fcl.arg(5, types.UFix64),
           // fcl.arg(address, types.Address),
@@ -36,12 +38,12 @@ transaction(amount: UFix64) {
         fcl.authorizations([fcl.currentUser().authorization]),
         fcl.payer(fcl.currentUser().authorization),
         fcl.limit(9999),
-      ])
-      await fcl.tx(response).onceSealed()
+      ]);
+      await fcl.tx(response).onceSealed();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <div className="App">
